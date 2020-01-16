b9d890d06b7adb0946d4df310787dca5314ce7fb
tong <tong@disktree.net>
2020-01-16 14:09:13 +0100

    sync

--- src/strobe/App.hx
+++ src/strobe/App.hx
@@ -3,99 +3,119 @@ package strobe;
 import js.Browser.console;
 import js.Browser.document;
 import js.Browser.window;
-import js.html.DivElement;
-import js.html.Element;
 import js.html.InputElement;
+import js.html.Element;
 import om.Time;
 
 class App {
 
-    static var element : DivElement;
-    static var controls : DivElement;
+	static var light : Element;
+	static var controls : Element;
+	
+	static var colorBackground = '#000';
+    static var colorFlash = '#fff';
+    static var flashInterval = 100;
+	static var flashDuration = 33;
 
-    static var backgroundColor : String;
-    static var flashColor : String;
-    static var flashInterval : Int;
-    static var flashDuration : Int;
-
-    static var flashing : Bool;
+	static var flashing = false;
     static var timeStart : Float;
     static var lastTimeFlash : Float;
 
-    static function update( time : Float ) {
+	static function update( time : Float ) {
 
         window.requestAnimationFrame( update );
 
-        var timeElapsed = time - lastTimeFlash;
+		var elapsed = Std.int( time - lastTimeFlash );
 
-        if( flashing ) {
-            if( timeElapsed > flashDuration ) {
+		if( flashing ) {
+			if( elapsed >= flashDuration ) {
+				light.style.backgroundColor = colorBackground;
                 flashing = false;
-                element.style.backgroundColor = backgroundColor;
+				lastTimeFlash = time;
             }
-        } else {
-            if( timeElapsed > flashInterval ) {
+		} else {
+			if( elapsed >= flashInterval ) {
+				light.style.backgroundColor = colorFlash;
                 flashing = true;
-                lastTimeFlash = time;
-                element.style.backgroundColor = flashColor;
+				lastTimeFlash = time;
             }
-        }
-    }
+		}
+	}
 
-    static function main() {
+	static function main() {
 
-        window.onload = function(e){
+		light = document.getElementById('light');
+		controls = document.getElementById('controls');
 
-            flashing = false;
-            timeStart = Time.now();
-            lastTimeFlash = timeStart;
+		var backgroundColorElement : InputElement = cast controls.querySelector( 'input[name="background-color"]' );
+		var intervalElement : InputElement = cast controls.querySelector( 'input[name="interval"]' );
+		var durationElement : InputElement = cast controls.querySelector( 'input[name="duration"]' );
+		var transitionElement : InputElement = cast controls.querySelector( 'input[name="transition"]' );
+		var flashColorElement : InputElement = cast controls.querySelector( 'input[name="flash-color"]' );
+		
+		backgroundColorElement.oninput = e -> colorBackground = e.target.value;
+		flashColorElement.oninput = e -> colorFlash = e.target.value;
 
-            element = cast document.querySelector( 'div.light' );
-            controls = cast document.querySelector( 'div.controls' );
+		intervalElement.oninput = e -> {
+			var v = Std.parseInt( e.target.value );
+			durationElement.max = Std.string(v);
+			flashInterval = v;
+		}
 
-            var backgroundColorElement : InputElement = cast document.querySelector( 'input[name="background-color"]' );
-            var intervalElement : InputElement = cast document.querySelector( 'input[name="interval"]' );
-            var durationElement : InputElement = cast document.querySelector( 'input[name="duration"]' );
-            var transitionElement : InputElement = cast document.querySelector( 'input[name="transition"]' );
-            var flashColorElement : InputElement = cast document.querySelector( 'input[name="flash-color"]' );
+		durationElement.oninput = e -> {
+			var v = Std.parseInt( e.target.value );
+			flashDuration = v;
+		}
+		
+		transitionElement.oninput = e -> {
+			var v = Std.parseInt( e.target.value ) / 1000;
+			light.style.transition = 'background-color '+v+'s';
+		}
+		
+		window.onkeydown = function(e) {
+			trace(e.keyCode);
+			switch e.keyCode {
+				case 89: //y
+					flashInterval = Std.int( Math.min( (flashInterval+10), 1000 ) );
+				case 88: //x
+					flashInterval = Std.int( Math.max( (flashInterval-10), 17 ) );
+				case 78: //n
+					flashDuration = Std.int( Math.max( (flashDuration-10), 17 ) );
+				case 77: //m
+					flashDuration = Std.int( Math.min( (flashDuration+10), 1000 ) );
+				case 32: // space
+					//
+			}
+		}
+		
+		timeStart = lastTimeFlash = Time.now();
+		window.requestAnimationFrame( update );
 
-            backgroundColorElement.oninput = e -> backgroundColor = e.target.value;
-            flashColorElement.oninput = e -> flashColor = e.target.value;
+		document.onmouseenter = e -> controls.classList.remove('hidden');
+		document.onmouseleave = e -> controls.classList.add('hidden');
 
-            intervalElement.oninput = e -> {
-                var v = Std.parseInt( e.target.value );
-                durationElement.max = Std.string(v);
-                flashInterval = v;
-            }
+		/*
+		window.onmousedown = function(e) {
+			//trace(e);
+		}
+		window.onmousemove = function(e) {
+			//trace(e);
+		}
+		window.onwheel = function(e) {
+			trace(e);
+		}
+		*/
 
-            durationElement.oninput = e -> {
-                var v = Std.parseInt( e.target.value );
-                flashDuration = v;
-            }
-
-            transitionElement.oninput = e -> {
-                var v = Std.parseInt( e.target.value ) / 1000;
-                element.style.transition = 'background-color '+v+'s';
-            }
-
-            backgroundColor = backgroundColorElement.value;
-            flashColor = flashColorElement.value;
-            flashInterval = Std.parseInt( intervalElement.value );
-            flashDuration = Std.parseInt( durationElement.value );
-
-            //window.addEventListener( 'resize', handleWindowResize, false );
-            //window.addEventListener( 'mousemove', handleMouseMove, false );
-            //window.addEventListener( 'click', handleClick, false );
-
-            window.requestAnimationFrame( update );
-
-            window.onblur = e -> {
-                //controls.classList.add('hidden');
-            }
-            window.onfocus = e -> {
-                //controls.classList.remove('hidden');
-            }
-        }
-    }
-
+		/*
+		om.audio.Microphone.get();
+		untyped navigator.getUserMedia( { audio: true },
+			function(e){
+				trace(e);
+			},
+			function(e) {
+				trace(e);
+			}
+		);
+		*/
+	}
 }
